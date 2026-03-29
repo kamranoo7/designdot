01323e008b14ca742d1eecaf963300da7e0496cd
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2026-03-29 11:01:04 +0530

    design-moble

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <title>HLS Global Group</title>
+      </head>
       <body>
         {children}
       </body>
